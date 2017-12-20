f17d92ba6b59c7ea97fa298526df1b5b546ef480
cj <thatcat13@gmail.com>
2017-12-20 11:58:04 -0800

    edit winner div

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
   var npcInterval = undefined;
   $('button#start-race').click(startGame);
 });
+function winnerName(kart){
+  switch (kart) {
+    case '#bigT':
+      return 'BIG TUUUUUNNNNAAAAA';
+      break;
+    case '#npc1':
+      return 'Vin Diesel';
+      break;
+    case '#npc2':
+      return 'Newman!!!';
+      break;
+    default:
+      console.log('Name not found!!');
+  }
+};
